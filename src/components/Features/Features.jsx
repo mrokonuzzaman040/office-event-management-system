ea2d337324a76b326634e81ab8ff8102601460d7
mrokonuzzaman040 <19201103040@cse.bubt.edu.bd>
2023-10-09 14:12:29 +0600

    Update Feature section

--- src/components/Features/Features.jsx
+++ src/components/Features/Features.jsx
@@ -5,35 +5,37 @@ import { MdAppRegistration } from "react-icons/md";
 
 const Features = () => {
     return (
-        <div className='grid md:grid-cols-2 p-3 md:gap-4 gap-10 mx-auto w-full max-w-screen-xl'>
-            <div className="">
-                <div className="card card-side bg-base-100 shadow-xl items-center">
-                    <figure><p className='text-7xl items-center text-white'><BiSolidMicrophoneAlt></BiSolidMicrophoneAlt></p></figure>
-                    <div className="card-body">
-                        <h2 className="text-start font-bold text-white text-2xl">SPEAKERS</h2>
-                        <p className='text-start text-white'>12 Professional Speakers</p>
+        <div className="">
+            <div className="text-center m-4">
+                <h2 className='text-4xl text-white'>Our Feature</h2>
+            </div>
+            <div className='grid md:grid-cols-2 p-3 md:gap-4 gap-10 mx-auto w-full max-w-screen-xl'>
+                <div className="">
+                    <div className="card card-side bg-base-100 shadow-xl items-center">
+                        <figure><p className='text-7xl items-center text-white'><BiSolidMicrophoneAlt></BiSolidMicrophoneAlt></p></figure>
+                        <div className="card-body">
+                            <h2 className="text-start font-bold text-white text-2xl">SPEAKERS</h2>
+                            <p className='text-start text-white'>12 Professional Speakers</p>
+                        </div>
                     </div>
                 </div>
-            </div>
 
-            <div className="">
-                <div className="card card-side bg-base-100 shadow-xl items-center">
-                    <figure><p className='text-7xl items-center text-white'><IoTicketSharp></IoTicketSharp></p></figure>
-                    <div className="card-body">
-                        <h2 className="text-start font-bold text-white text-2xl">TICKETS</h2>
-                        <p className='text-start text-white'>12000 Tickets Remaining</p>
+                <div className="">
+                    <div className="card card-side bg-base-100 shadow-xl items-center">
+                        <figure><p className='text-7xl items-center text-white'><IoTicketSharp></IoTicketSharp></p></figure>
+                        <div className="card-body">
+                            <h2 className="text-start font-bold text-white text-2xl">TICKETS</h2>
+                            <p className='text-start text-white'>12000 Tickets Remaining</p>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div className="card card-side bg-base-100 shadow-xl items-center">
-                <figure><p className='text-7xl items-center text-white'><MdAppRegistration></MdAppRegistration></p></figure>
-                <div className="card-body">
-                    <h2 className="text-start font-bold text-white text-2xl">REGISTRATION</h2>
-                    <p className='text-start text-white'>4 Days Remaining</p>
+                <div className="card card-side bg-base-100 shadow-xl items-center">
+                    <figure><p className='text-7xl items-center text-white'><MdAppRegistration></MdAppRegistration></p></figure>
+                    <div className="card-body">
+                        <h2 className="text-start font-bold text-white text-2xl">REGISTRATION</h2>
+                        <p className='text-start text-white'>4 Days Remaining</p>
+                    </div>
                 </div>
-            </div>
-            <div className="">
-
             </div>
         </div>
     );
